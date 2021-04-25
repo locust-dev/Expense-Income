aff9afcc97b2d525a9d760b5c27e2b790de5fdd6
locust-dev <rrrr.ty30@gmail.com>
2021-04-26 01:53:41 +0300

    Fixes scroll view

--- Expense&Income/DataModel/DataModel.swift
+++ Expense&Income/DataModel/DataModel.swift
@@ -35,7 +35,7 @@ struct ExpensesAndIncomes {
     }
     
     static func getGroup() -> ExpensesAndIncomes {
-        ExpensesAndIncomes(expenses: [1000, 500, 777, 300], incomes: [900, 3500, 4200], catForExpenses: ["Машина", "Досуг", "Косметология", "Для дома"], catForIncomes: ["С фриланса", "Майнинг", "Зарплата"])
+        ExpensesAndIncomes(expenses: [1000, 500, 777, 300], incomes: [900, 3500, 4200, 300], catForExpenses: ["Машина", "Досуг", "Косметология", "Для дома"], catForIncomes: ["С фриланса", "Долг", "Зарплата", "Другое"])
     }
     
 }
